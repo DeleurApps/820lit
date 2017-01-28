e8b0357183c61d518adcdb62a436ffaa5a782f98
John Zeiders <zeiders@usc.edu>
2017-01-27 18:43:32 -0800

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,20 @@ $("#test").click(function() {
 	firebase.database().ref("/").once('value').then(function(snapshot) {
 		settings = snapshot.val();
 	});
-  console.log("fuck");
+	console.log("fuck");
 });
+$('#redSlider').slider()
+	.on("slide", function(num) {
+		var value = num.value;
+		firebase.database().ref("/").update({
+			R: value
+		});
+	});
+
+$('#brightSlider').slider()
+	.on("slide", function(num) {
+		var value = num.value;
+		firebase.database().ref("/").update({
+			brightness: value
+		});
+	});
